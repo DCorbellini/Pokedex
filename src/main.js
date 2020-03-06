2bcc1f0d8426b140740bc051bd24fcb42ad0251c
DCorbellini <DCorbellini@users.noreply.github.com>
2020-03-06 19:51:37 -0300

    Fetchs nombres y los muestra en forma de texto

--- src/main.js
+++ src/main.js
@@ -2,9 +2,24 @@ const POKEAPI = "https://pokeapi.co/api/v2/pokemon/"
 
 function fetchPokemon () {
 fetch(`${POKEAPI}`).then(response => response.json())
+.then (responseJSON => {
+    fetchName(responseJSON)
+})
 .catch(err => {
     console.log ('Error: ', err)
 })
+}
+
+function fetchName(responseJSON){
+    let nombrePokemon
+    let $main = document.querySelector("#main")
+
+    for (i=0; i < responseJSON.results.length; i++){
+        nombrePokemon = responseJSON.results[i].name;
+        let $nuevoP = document.createElement('p')
+        $nuevoP.textContent = nombrePokemon
+        $main.appendChild($nuevoP)
+    }
 }
 
 fetchPokemon ()
