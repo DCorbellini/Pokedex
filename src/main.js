d8aed8355ad412e462f86557a82bb9958c143fd9
DCorbellini <DCorbellini@users.noreply.github.com>
2020-03-08 20:48:16 -0300

    Agregar click funcional a las cartas

--- src/main.js
+++ src/main.js
@@ -2,48 +2,60 @@
 
 const POKEAPI_BASE = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=40"
 
-function fetchPokemon (URL) {
+function fetchPokemon(URL) {
 
     $.ajax({
         method: "GET",
         url: URL,
         success: response => {
-            agregarCarta (response)
-            contarCartas ()
+            agregarCarta(response)
+            contarCartas()
         },
 
         error: () => {
-            mensajeError ("ajax")
+            mensajeError("ajax")
         }
-        
+
     })
-    
+
 }
 
-function agregarCarta (response) {
+function agregarCarta(response) {
     const $contenedorCartas = $("#contenedor-cartas")
     response.results.forEach(pokemon => {
         $contenedorCartas.append(`<div class="col m-1 rounded carta">${pokemon.name}</div>`)
     });
 }
 
-function contarCartas () {
-    if($(".carta").length===40){
+function contarCartas() {
+    if ($(".carta").length === 40) {
         $("#cargando").detach()
-    }else(
+        manejarCartas()
+    } else (
         mensajeError('contarCartas')
     )
 }
 
-function mensajeError (err){
-    switch(err) {
+function manejarCartas() {
+    $('.carta').click(e => {
+        mostarInfoPokemon(e.target.innerText)
+    })
+}
+
+function mostarInfoPokemon(pokemon) {
+    console.log(pokemon)
+}
+
+
+function mensajeError(err) {
+    switch (err) {
         case 'ajax':
             $('#cargando').text('Error al cargar la informacion de la API')
             break
         case 'contarCartas':
             $('#cargando').text('Error al mostrar los pokemones')
             break
-    } 
+    }
 }
 
-fetchPokemon (POKEAPI_BASE)
+fetchPokemon(POKEAPI_BASE)
